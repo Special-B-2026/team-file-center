9a99377b220451fed571816fd6d3b4068804485e
Special-B-2026 <super7.la.bu.bu@gmail.com>
2026-07-18 20:19:12 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 let current = FILE_LIBRARY;
 
-const list = document.getElementById('fileList');
+const list = document.getElementById("fileList");
 
-document.getElementById('count').innerText = FILE_LIBRARY.length;
+const count = document.getElementById("count");
+if(count){
+    count.innerText = FILE_LIBRARY.length;
+}
 
-
-const pdf = document.getElementById('pdfCount');
+const pdf = document.getElementById("pdfCount");
 if(pdf){
-    pdf.innerText =
-    FILE_LIBRARY.filter(f => f.type === "PDF").length;
+    pdf.innerText = FILE_LIBRARY.filter(f=>f.type==="PDF").length;
 }
 
-
-const excel = document.getElementById('excelCount');
+const excel = document.getElementById("excelCount");
 if(excel){
-    excel.innerText =
-    FILE_LIBRARY.filter(f => f.type === "EXCEL").length;
+    excel.innerText = FILE_LIBRARY.filter(f=>f.type==="EXCEL").length;
 }
 
-
-const word = document.getElementById('wordCount');
+const word = document.getElementById("wordCount");
 if(word){
-    word.innerText =
-    FILE_LIBRARY.filter(f => f.type === "WORD").length;
+    word.innerText = FILE_LIBRARY.filter(f=>f.type==="WORD").length;
 }
-
 
 
 function show(data){
@@ -34,19 +30,23 @@ function show(data){
     data.forEach(f=>{
 
         list.innerHTML += `
+
         <div class="card">
 
             <h3>${f.name}</h3>
 
-            <p>${f.group} | ${f.type}</p>
+            <p>
+            ${f.group || ""}
+            |
+            ${f.type}
+            </p>
 
-            <a href="${f.url}" download>
-                <button>
-                Download
-                </button>
+            <a href="${f.url}" target="_blank" download>
+            ดาวน์โหลดไฟล์
             </a>
 
         </div>
+
         `;
 
     });
@@ -57,16 +57,20 @@ function show(data){
 show(current);
 
 
+const search = document.getElementById("search");
 
-document.getElementById('search').oninput = e=>{
+if(search){
 
-show(
-FILE_LIBRARY.filter(
-f=>f.name.toLowerCase()
-.includes(
-e.target.value.toLowerCase()
-)
-)
-);
+search.oninput = e=>{
+
+    const text = e.target.value.toLowerCase();
+
+    const result = FILE_LIBRARY.filter(f=>
+        f.name.toLowerCase().includes(text)
+    );
+
+    show(result);
 
 };
+
+}
