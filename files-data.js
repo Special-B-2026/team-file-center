e2d9bf9661c0ed4502ad7d3c58263d94fdbb3cb7
Special-B-2026 <super7.la.bu.bu@gmail.com>
2026-07-23 09:29:16 +0700

    Add files via upload

--- files-data.js
+++ files-data.js
@@ -2,86 +2,44 @@ const FILE_LIBRARY = [
 
 {
 name:"ใบคำขอสินเชื่อ.pdf",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Loan_Request.pdf",
+url:"files/Loan_Request.pdf",
 type:"PDF",
 group:"แบบฟอร์ม"
 },
 
 {
 name:"ใบคำขอสินเชื่อบุคคล.pdf",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Personal_Loan_Request.pdf",
+url:"files/Personal_Loan_Request.pdf",
 type:"PDF",
 group:"แบบฟอร์ม"
 },
 
 {
-name:"แบบคำนวณรายได้",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Income_Calculation.xlsx",
+name:"แบบคำนวณรายได้.xlsx",
+url:"files/Income_Calculation.xlsx",
 type:"EXCEL",
 group:"คำนวณ"
 },
 
 {
 name:"สรุปรายได้ (Sum Statement)",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Income_Summary.xlsx",
+url:"files/Income_Summary.xlsx",
 type:"EXCEL",
 group:"คำนวณ"
 },
 
 {
 name:"ดอกเบี้ย.pdf",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Interest_Rate.pdf",
+url:"files/Interest_Rate.pdf",
 type:"PDF",
 group:"ดอกเบี้ย"
 },
 
 {
 name:"ลดดอกเบี้ย.xlsx",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Interest_Reduction.xlsx",
+url:"files/Interest_Reduction.xlsx",
 type:"EXCEL",
 group:"ดอกเบี้ย"
-},
-
-{
-name:"MLTA.xlsx",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/MLTA_Calculation.xlsx",
-type:"EXCEL",
-group:"ประกัน"
-},
-
-{
-name:"MLTA EXTRA.xlsx",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/MLTA_Extra.xlsx",
-type:"EXCEL",
-group:"ประกัน"
-},
-
-{
-name:"MRTA.xlsx",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/MRTA_Calculation.xlsx",
-type:"EXCEL",
-group:"ประกัน"
-},
-
-{
-name:"MRTA EXTRA.xlsx",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/MRTA_Extra.xlsx",
-type:"EXCEL",
-group:"ประกัน"
-},
-
-{
-name:"ค่าคอมมิชชั่น.pdf",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Commission.pdf",
-type:"PDF",
-group:"ค่าตอบแทน"
-},
-
-{
-name:".ใบคำขอเปลี่ยนแปลงข้อมูลลูกค้า.pdf",
-url:"https://raw.githubusercontent.com/Special-B-2026/team-file-center/main/Customer_Data_Change_Request.pdf",
-type:"PDF",
-group:"แบบฟอร์ม"
 }
 
 ];
