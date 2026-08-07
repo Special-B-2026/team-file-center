36afeab3e2a745f275ec32ac0634a47a3639c837
Special-B-2026 <super7.la.bu.bu@gmail.com>
2026-08-07 23:05:48 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ ${file.isNew ? '<span class="new-badge">NEW</span>' : ''}
 
         <a
 class="download"
-href="files/${file.fileName}"
+href="./files/${file.fileName}"
 target="_blank"
 onclick="countDownload('${file.fileName}')">
 ดาวน์โหลด
