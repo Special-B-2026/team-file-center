282318bb97d6ffd974397f8e4e628e86022ff4a2
Special-B-2026 <super7.la.bu.bu@gmail.com>
2026-08-13 10:43:12 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -34,16 +34,10 @@ self.addEventListener("activate", event => {
 
 
 self.addEventListener("fetch", event => {
-
-  if (event.request.url.includes("/files/")) {
-    return;
-  }
-
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    .then(response => {
+      return response || fetch(event.request);
+    })
   );
-
 });
